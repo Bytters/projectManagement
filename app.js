9403800e717d33744c889667a9bcf474aaf923a6
Bytter <67333597+Bytters@users.noreply.github.com>
2024-03-15 13:35:59 -0300

    algumas mudanças

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import { checkbox, createPrompt, input, select } from "@inquirer/prompts"
 import { Create } from "./utils/create/create.js"
 import { See } from "./utils/see/see.js"
+import { Edit } from "./utils/edit/Edit.js"
 import fs from "fs"
 
 const answer = select({
@@ -8,6 +9,7 @@ const answer = select({
     choices: [
         { name: "Ver Tarefa", value: "see" },
         { name: "Criar Tarefa", value: "create" },
+        { name: "Editar Tarefa", value: "edit" },
     ],
 })
 
@@ -27,6 +29,9 @@ const answerResolved = answer.then(async (Answers) => {
             break
         case "see":
             See(Answers, fileDirectory)
+            break
+        case "edit":
+            Edit(fileDirectory, createTask)
             break
         default:
             console.log("Escolha alguma opção")
